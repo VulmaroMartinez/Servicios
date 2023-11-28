cfd6c41918db6a982aa098239f99d6cb5d07741f
VulmaroMartinez <20223tn069@utez.edu.mx>
2023-11-28 00:10:52 -0600

    Front y security

--- src/main/java/mx/edu/utez/IntegradoraServicios/security/SecurityConfig.java
+++ src/main/java/mx/edu/utez/IntegradoraServicios/security/SecurityConfig.java
@@ -1,14 +1,14 @@
 package mx.edu.utez.IntegradoraServicios.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SegurityConfig {
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
